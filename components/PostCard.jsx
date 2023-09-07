f5e2a2c42ddbcf23e1061391dd79c1075b99d47d
James <jjguevarag@gmail.com>
2023-09-07 14:17:05 -0600

    add new funcionality

---
+++ components/PostCard.jsx
@@ -0,0 +1,17 @@
+"use client";
+
+function PostCard({post}){
+  return(
+    <div>
+    <h3>{post.id}. {post.title}</h3>
+    <p>{post.body}</p>
+    <button onClick={() => {
+      alert("Click Funcionando")
+    }} >
+    Click
+    </button>
+  </div>
+  )
+}
+
+export default PostCard;
