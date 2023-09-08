9e729cfe77755f3a165827e93ce138f94d567078
James <jjguevarag@gmail.com>
2023-09-07 18:24:30 -0600

    .

--- app/about/page.jsx
+++
@@ -1,9 +0,0 @@
-export const metadata = {
-  title: "Acerca de Nosotros"
-}
-
-function AboutPage(){
-  return <h1>About</h1>
-}
-
-export default AboutPage;
--- app/tienda/categorias/computadoras/page.jsx
+++
@@ -1,7 +0,0 @@
-export const metadata = {
-  title: "Computadoras"
-}
-
-export default function PageComputer (){
-  return <h1>Computadoras</h1>
-}
