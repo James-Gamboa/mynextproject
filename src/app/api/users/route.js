b1a0f92603d043d2e051af1db27289f7a3a04ba8
James <jjguevarag@gmail.com>
2023-09-12 11:23:43 -0600

    update .gitignore

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,27 +1,34 @@
 import { NextResponse } from "next/server";
 
+console.log(process.env.TOKEN);
+console.log(process.env.SECRET_KEY);
+
 export async function GET() {
-  const res = await fetch ("https://jsonplaceholder.typicode.com/users")
-  const data = await res.json()
-  return  NextResponse.json(data);
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/users?apikey=" + process.env.TOKEN
+  );
+  console.log("https://jsonplaceholder.typicode.com/users?apikey=" + process.env.TOKEN)
+  const data = await res.json();
+  return NextResponse.json(data);
 }
 
+export async function POST() {
+  const { nombre, apellido } = await request.json();
+  console.log(nombre, apellido);
 
-export function POST() {
-  return  NextResponse.json({
-    message: "Creando Datos!"
+  return NextResponse.json({
+    message: "Creando Datos!",
   });
 }
 
-
 export function PUT() {
-  return  NextResponse.json({
-    message: "Actualizando Datos!"
+  return NextResponse.json({
+    message: "Actualizando Datos!",
   });
 }
 
 export function DELETE() {
-  return  NextResponse.json({
-    message: "Eliminando Datos!"
+  return NextResponse.json({
+    message: "Eliminando Datos!",
   });
 }
